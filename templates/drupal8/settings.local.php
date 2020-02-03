d1dae0ebee6696f6b77c63e569972a2557d02fc4
Meg Claypool <margaret.claypool@gmail.com>
2020-02-03 09:53:31 -0800

    Change D8 settings.local placeholder values to match script

--- templates/drupal8/settings.local.php
+++ templates/drupal8/settings.local.php
@@ -4,9 +4,9 @@
 if (!defined('PANTHEON_ENVIRONMENT')) {
   // Database.
   $databases['default']['default'] = array(
-    'database' => 'LOCAL_DATABASE_NAME_PLACEHOLDER',
-    'username' => 'MYSQL_USERNAME_PLACEHOLDER',
-    'password' => 'MYSQL_PASSWORD_PLACEHOLDER',
+    'database' => 'DB_NAME_PLACEHOLDER',
+    'username' => 'DB_USER_PLACEHOLDER',
+    'password' => 'DB_PASS_PLACEHOLDER',
     'host' => 'localhost',
     'driver' => 'mysql',
     'port' => 3306,
@@ -19,41 +19,41 @@ if (!defined('PANTHEON_ENVIRONMENT')) {
 
   ini_set('session.gc_maxlifetime', 0);
   ini_set('session.cookie_lifetime', 0);
-  
+
   /**
    * Disable CSS and JS aggregation.
    */
   $config['system.performance']['css']['preprocess'] = FALSE;
   $config['system.performance']['js']['preprocess'] = FALSE;
-  
+
   /**
    * Disable/bypass the Drupal Render API cache
    */
   $settings['cache']['bins']['render'] = 'cache.backend.null';
 
-  
+
   $settings['hash_salt'] = 'my_random_hash';
   $settings['container_yamls'][] = __DIR__ . '/services.local.yml';
 
-  /** 
+  /**
    * Error Logging
    */
   // error_reporting(E_ALL);
   // ini_set('display_errors', TRUE);
   // ini_set('display_startup_errors', TRUE);
   // $config['system.logging']['error_level'] = 'verbose';
-  
-  
+
+
   /**
    * Set Kint's max levels when using Devel and Kint in Drupal 8:
    * Only uncomment if devel kint is installed and enabled!
    */
   // require_once DRUPAL_ROOT . '/modules/contrib/devel/kint/kint/Kint.class.php';
   // Kint::$maxLevels = 3;
-  
-  
+
+
   /**
-  *For Drupal 8 with subfolder install: 
+  *For Drupal 8 with subfolder install:
   */
   // $base_url = 'http://site_folder_name.whatever_domain';
   // if (isset($GLOBALS['request']) && '/web/index.php' === $GLOBALS['request']->server->get('SCRIPT_NAME')) {
